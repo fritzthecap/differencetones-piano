d0b4abe588d2a3d185570271657e2d725f485e93
fridell <fritz.ritzberger@chello.at>
2025-09-23 19:17:33 +0200

    Improved FrameStarter to size window.

--- src/main/java/fri/music/swingutils/FrameStarter.java
+++ src/main/java/fri/music/swingutils/FrameStarter.java
@@ -8,26 +8,43 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
+/**
+ * Do not use <code>new JFrame()</code> because closing frames
+ * can not be controlled that way and sound resources may remain
+ * occupied in operating system.
+ */
 public class FrameStarter
 {
     private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private static int titlebarHeight = 32;
-    private static int cascadeX = titlebarHeight, cascadeY = titlebarHeight;
     
     private static List<JFrame> frames = new ArrayList<>();
     private static boolean exiting = false;
     
+    /** Runs an EXIT_ON_CLOSE frame. */
     public static JFrame start(String title, JComponent content) {
-        return start(title, true, content, null);
+        return start(title, content, null);
     }
     
-    public static JFrame start(String title, boolean exitOnClose, JComponent content, WindowListener windowListener) {
+    /** Runs an EXIT_ON_CLOSE frame. */
+    public static JFrame start(String title, JComponent content, Dimension dimension) {
+        return start(title, true, content, null, dimension);
+    }
+    
+    /** Runs a frame configured by given parameters. */
+    public static JFrame start(
+            String title, 
+            boolean exitOnClose, 
+            JComponent content, 
+            WindowListener windowListener,
+            Dimension dimension)
+    {
         final JFrame frame = new JFrame(title);
-        frame.getContentPane().add(content);
+        frame.getContentPane().add(Objects.requireNonNull(content));
         frame.setDefaultCloseOperation(exitOnClose ? JFrame.EXIT_ON_CLOSE : JFrame.DISPOSE_ON_CLOSE);
         
         if (windowListener != null)
@@ -53,35 +70,43 @@ public class FrameStarter
             }
         });
         
-        frame.pack();
-        final Dimension size = frame.getSize();
-        if (size.width < 30)
-            size.width = 200;
-        if (size.height < 30 + titlebarHeight) // title-bar height counts
-            size.height = 200;
-        frame.setSize(size);
+        if (dimension == null)
+            frame.pack();
+        else
+            frame.setSize(dimension);
+        
+        ensureVisibleSize(frame); // else tiny frame may be hard to find on screen
         
         final JFrame previousFrame = (frames.size() > 0) ? frames.get(frames.size() - 1) : null;
-        frame.setLocation(getNextCascadingPoint(previousFrame, frame));
+        frame.setLocation(nextCascadingPoint(previousFrame, frame));
         frame.setVisible(true);
         
         frames.add(frame);
         return frame;
     }
 
-    private static Point getNextCascadingPoint(JFrame previousFrame, JFrame newFrame)    {
+    private static void ensureVisibleSize(JFrame frame) {
+        final Dimension size = frame.getSize();
+        if (size.width < 30)
+            size.width = 200;
+        if (size.height < 30 + titlebarHeight) // title-bar height counts
+            size.height = 200;
+        frame.setSize(size);
+    }
+
+    private static Point nextCascadingPoint(JFrame previousFrame, JFrame newFrame)    {
         final Dimension d = newFrame.getSize();
         final Point p;
-        if (previousFrame == null) {
+        if (previousFrame == null) { // go to middle of the screen
             p = new Point(
                     screenSize.width / 2 - d.width / 2,
                     screenSize.height / 2 - d.height / 2);
         }
-        else {
+        else { // cascade to last location
             final Point previous = previousFrame.getLocation();
             p = new Point(
-                    previous.x + cascadeX,
-                    previous.y + cascadeY);
+                    previous.x + titlebarHeight,
+                    previous.y + titlebarHeight);
         }
         return ensureWithinScreen(p, d);
     }
@@ -95,7 +120,7 @@ public class FrameStarter
     }
 
     
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         final JPanel content = new JPanel();
         final WindowListener closeListener = new WindowAdapter() {
             @Override
@@ -103,8 +128,8 @@ public class FrameStarter
                 System.out.println("My windowClosing was called on "+((JFrame) e.getSource()).getTitle());
             }
         };
-        FrameStarter.start("DISPOSE Frame", false, content, closeListener);
-        FrameStarter.start("EXIT 1 Frame", true, content, closeListener);
-        FrameStarter.start("EXIT 2 Frame", true, content, closeListener);
-    }
+        FrameStarter.start("DISPOSE Frame", false, content, closeListener, null);
+        FrameStarter.start("EXIT 1 Frame", true, content, closeListener, null);
+        FrameStarter.start("EXIT 2 Frame", true, content, closeListener, null);
+    }*/
 }
