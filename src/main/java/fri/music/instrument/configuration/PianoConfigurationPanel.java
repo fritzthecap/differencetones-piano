0915b17a40e7ba64e1a39584ac6c52694a82f7cb
fridell <fritz.ritzberger@chello.at>
2025-10-07 18:18:05 +0200

    Reefactoring for configuration UI of tone.systems.

--- src/main/java/fri/music/instrument/configuration/PianoConfigurationPanel.java
+++ src/main/java/fri/music/instrument/configuration/PianoConfigurationPanel.java
@@ -1,60 +1,36 @@
 package fri.music.instrument.configuration;
 
-import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import fri.music.ScaleTypes;
-import fri.music.ToneSystem;
 import fri.music.instrument.PianoWithSound;
-import fri.music.swingutils.layout.SmartComboBox;
 import fri.music.utils.StringUtil;
 
 /**
  * A panel that lets configure most options for different
  * kinds of Piano implementations.
  */
-public class PianoConfigurationPanel
+public class PianoConfigurationPanel extends ToneRangeConfigurationPanel
 {
-    /** The user-interface panel to add to some frame or dialog. */
-    public final JPanel panel;
-    
     protected final JPanel displayOptionsPanel;
     
-    private JSlider octaves;
-    private JComboBox<String> lowestToneBaseName;
-    private JComboBox<String> lowestToneScaleName;
-    private JSlider lowestToneOctave;
     private JSlider blackKeyPixelWidth;
     private JCheckBox showIpnNameOnKey;
     private JCheckBox showMidiNumberAsTooltip;
     private JCheckBox colouredOctaves;
     
     public PianoConfigurationPanel(PianoWithSound.Configuration configuration) {
+        super(
+                (configuration != null) ? configuration.octaves : 0,
+                (configuration != null) ? StringUtil.getUntilFirstNumber(configuration.lowestToneIpnName) : null,
+                (configuration != null) ? configuration.lowestToneOctaveBasedOnC : 0
+            );
+
         // field construction
         buildPianoConfigurationFields(configuration);
-        
-        // fields layout
-        final JPanel lowestTonePanel = new JPanel();
-        lowestTonePanel.setLayout(new BoxLayout(lowestTonePanel, BoxLayout.X_AXIS));
-        lowestTonePanel.setBorder(BorderFactory.createTitledBorder(
-                        BorderFactory.createLineBorder(Color.LIGHT_GRAY, 4, true),
-                        "Lowest Tone"));
-        lowestTonePanel.add(lowestToneBaseName);
-        lowestTonePanel.add(lowestToneScaleName);
-        lowestTonePanel.add(lowestToneOctave);
         
         this.displayOptionsPanel = new JPanel();
         displayOptionsPanel.setLayout(new BoxLayout(displayOptionsPanel, BoxLayout.Y_AXIS));
@@ -65,10 +41,6 @@ public class PianoConfigurationPanel
         moveLeftOptionsPanel.setBorder(BorderFactory.createTitledBorder("Display Options"));
         moveLeftOptionsPanel.add(displayOptionsPanel);
 
-        this.panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(octaves);
-        panel.add(lowestTonePanel);
         panel.add(moveLeftOptionsPanel);
         panel.add(blackKeyPixelWidth);
     }
@@ -76,8 +48,8 @@ public class PianoConfigurationPanel
     /** @return a piano configuration built from all UI fields. */
     public PianoWithSound.Configuration getPianoConfiguration() {
         return new PianoWithSound.Configuration(
-            octaves.getValue(),
-            ""+lowestToneBaseName.getSelectedItem()+lowestToneOctave.getValue(),
+            getOctaves(),
+            getLowestToneIpnName(),
             false,
             blackKeyPixelWidth.getValue(),
             showIpnNameOnKey.isSelected(),
@@ -90,79 +62,9 @@ public class PianoConfigurationPanel
     private void buildPianoConfigurationFields(PianoWithSound.Configuration configurationParam) {
         final PianoWithSound.Configuration configuration;
         if (configurationParam == null)
-            configuration = new PianoWithSound.Configuration(7, "C2");
+            configuration = new PianoWithSound.Configuration(DEFAULT_OCTAVES, DEFAULT_LOWEST_TONE_IPNNAME);
         else
             configuration = configurationParam;
-        
-        final ChangeListener octavesListener = new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                if (e != null && ((JSlider) e.getSource()).getValueIsAdjusting())
-                    return;
-                final int startOctave = lowestToneOctave.getValue();
-                final int maximumPossibleOctaves = ToneSystem.MAXIMUM_OCTAVES - startOctave;
-                final boolean isMaximum = (octaves.getValue() == octaves.getMaximum());
-                octaves.setMaximum(maximumPossibleOctaves);
-                // try to keep a good value
-                if (isMaximum && maximumPossibleOctaves <= configuration.octaves)
-                    octaves.setValue(maximumPossibleOctaves);
-            }
-        };
-        
-        this.octaves = new JSlider(1, ToneSystem.MAXIMUM_OCTAVES, configuration.octaves);
-        octaves.setToolTipText("The Number of Octaves above the Starting Octave");
-        octaves.setBorder(BorderFactory.createTitledBorder("Number of Octaves (Maximum "+ToneSystem.MAXIMUM_OCTAVES+")"));
-        octaves.setSnapToTicks(true);
-        octaves.setPaintLabels(true);
-        octaves.setPaintTicks(true);
-        octaves.setMajorTickSpacing(1);
-        octaves.addChangeListener(octavesListener);
-        
-        final List<String> ipnBaseNamesList = Stream.of(ToneSystem.IPN_BASE_NAMES)
-            .filter(ipnName -> ipnName.indexOf(ToneSystem.SHARP_CHAR) < 0)
-            .collect(Collectors.toList());
-        final String[] ipnBaseNames = ipnBaseNamesList.toArray(new String[ipnBaseNamesList.size()]);
-        this.lowestToneBaseName = new SmartComboBox(ipnBaseNames);
-        lowestToneBaseName.setBorder(BorderFactory.createTitledBorder("Key"));
-        lowestToneBaseName.setToolTipText("Leftmost (and Rightmost) Tone of the Keyboard");
-        
-        final Set<String> scaleNamesSet = ScaleTypes.scaleToStartNote.sequencedKeySet();
-        final String[] scaleNames = scaleNamesSet.toArray(new String[scaleNamesSet.size()]);
-        this.lowestToneScaleName = new SmartComboBox(scaleNames);
-        lowestToneScaleName.setBorder(BorderFactory.createTitledBorder("or Scale"));
-        lowestToneScaleName.setToolTipText("Modal Scale Type");
-        
-        lowestToneBaseName.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final String ipnBaseName = (String) lowestToneBaseName.getSelectedItem();
-                final String scaleName = ScaleTypes.scaleName(ipnBaseName);
-                lowestToneScaleName.setSelectedItem(scaleName);
-            }
-        });
-        lowestToneScaleName.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final String scaleName = (String) lowestToneScaleName.getSelectedItem();
-                final String ipnBaseName = ScaleTypes.scaleToStartNote.get(scaleName);
-                lowestToneBaseName.setSelectedItem(ipnBaseName);
-            }
-        });
-        
-        final String lowestToneWithoutOctave = StringUtil.getUntilFirstNumber(configuration.lowestToneIpnName);
-        lowestToneBaseName.setSelectedItem(lowestToneWithoutOctave);
-        
-        this.lowestToneOctave = new JSlider(
-                ToneSystem.LOWEST_OCTAVE, 
-                ToneSystem.MAXIMUM_OCTAVES - 1, 
-                configuration.lowestToneOctaveBasedOnC);
-        lowestToneOctave.setBorder(BorderFactory.createTitledBorder("Start Octave"));
-        lowestToneOctave.setToolTipText("The Lowest (Leftmost) Octave of the Keyboard");
-        lowestToneOctave.setSnapToTicks(true);
-        lowestToneOctave.setPaintLabels(true);
-        lowestToneOctave.setPaintTicks(true);
-        lowestToneOctave.setMajorTickSpacing(1);
-        lowestToneOctave.addChangeListener(octavesListener);
         
         this.blackKeyPixelWidth = new JSlider(4, 60, configuration.blackKeyWidth);
         blackKeyPixelWidth.setBorder(BorderFactory.createTitledBorder("Black Key Pixel Width"));
@@ -182,7 +84,5 @@ public class PianoConfigurationPanel
         this.colouredOctaves = new JCheckBox("Coloured Octaves");
         colouredOctaves.setToolTipText("Show Every Octave with a Different Color");
         colouredOctaves.setSelected(configuration.colouredOctaves);
-        
-        octavesListener.stateChanged(null);
     }
 }
