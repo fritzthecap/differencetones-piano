df1036f7d8be0eeca8c491809303c64291eca41e
fridell <fritz.ritzberger@chello.at>
2025-10-20 19:20:52 +0200

    Added method for wider line of titled border.

--- src/main/java/fri/music/utils/swing/BorderUtil.java
+++ src/main/java/fri/music/utils/swing/BorderUtil.java
@@ -1,5 +1,6 @@
 package fri.music.utils.swing;
 
+import java.awt.Color;
 import java.awt.Font;
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
@@ -10,6 +11,18 @@ public final class BorderUtil
     /** @return a TitledBorder with bigger font. */
     public static Border titledBorder(String title, float fontSizeIncrement) {
         final TitledBorder titledBorder = BorderFactory.createTitledBorder(title);
+        return configureBorder(fontSizeIncrement, titledBorder);
+    }
+    
+    /** @return a TitledBorder with bigger font and thicker line. */
+    public static Border titledBorder(String title, float fontSizeIncrement, int linePixelWidth) {
+        final TitledBorder titledBorder = BorderFactory.createTitledBorder(
+                BorderFactory.createLineBorder(Color.LIGHT_GRAY, linePixelWidth, true),
+                title);
+        return configureBorder(fontSizeIncrement, titledBorder);
+    }
+
+    private static Border configureBorder(float fontSizeIncrement, TitledBorder titledBorder) {
         final Font titleFont = titledBorder.getTitleFont();
         titledBorder.setTitleFont(titleFont.deriveFont(titleFont.getSize2D() + fontSizeIncrement));
         return titledBorder;
